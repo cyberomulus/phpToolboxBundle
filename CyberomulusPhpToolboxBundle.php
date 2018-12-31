0908760ab925c06061902634e8b430a9ad0d4ce0
Cyberomulus <me@cyberomulus.me>
2018-12-31 15:01:08 +0100

    Bundle created

---
+++ CyberomulusPhpToolboxBundle.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * This file is part of the phpToolBoxBundle package.
+ *
+ * (c) Brack Romain <http://www.cyberomulus.me>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace cyberomulus\phpToolboxBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author	Brack Romain <http://www.cyberomulus.me>
+ */
+class CyberomulusPhpToolboxBundle extends Bundle
+    {
+    
+    }
