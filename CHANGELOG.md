c8a2082603fa5ecd3b1e6ddd25b61dd5b89a5419
cyberomulus <cyberomulus.me@gmail.com>
2020-03-31 00:22:33 +0200

    Change email for contact in doc (#3)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 
 * Add Changelog file
 * Update Symfony version required to `4.4.*`
+* Change email for contact in doc
 
 # 1.0 (2019-01-02)
 
--- CyberomulusPhpToolboxBundle.php
+++ CyberomulusPhpToolboxBundle.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of the phpToolBoxBundle package.
  *
- * (c) Brack Romain <http://www.cyberomulus.me>
+ * (c) Brack Romain <cyberomulus.me@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -13,7 +13,7 @@ namespace Cyberomulus\PhpToolboxBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
- * @author	Brack Romain <http://www.cyberomulus.me>
+ * @author	Brack Romain <cyberomulus.me@gmail.com>
  */
 class CyberomulusPhpToolboxBundle extends Bundle
     {
